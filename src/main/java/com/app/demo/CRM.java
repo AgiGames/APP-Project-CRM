70d0c73c67092c56ac315d98b7e0b467bc8d2124
AgiGames <agilesh1081@gmail.com>
2024-09-04 09:58:18 +0530

    Small Update

--- src/main/java/com/app/demo/CRM.java
+++ src/main/java/com/app/demo/CRM.java
@@ -4,11 +4,13 @@ package com.app.demo;
     USE THE FOLLOWING LINE IF YOU ARE GOING TO USE MongoDB FOR ANY PURPOSE:
     ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger("org.mongodb.driver").setLevel(Level.ERROR);
 */
+import java.io.File;
 
 class CRM {
 
     // path to the local database
-    public static String localDatabasePath = "D:\\Education\\Java\\APP-Project-CRM\\database";
+    public static String localDatabasePath = System.getProperty("user.dir") + "\\" + "database";
+    //public static String localDatabasePath = "D:\\Education\\Java\\APP-Project-CRM\\database";
     //public static String localDatabasePath = "s3:\\\\ai-2-crm\\database";
 
 }
